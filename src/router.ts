4835fc0f29e2ad832d64ced849fc5db8bbf065d2
EjiOsa <powder_junky@msn.com>
2019-09-09 08:37:17 +0900

    SPAの準備 ルーティングで各ページを設定。 その受け取り側のApp.vueを修正。

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,10 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+// import Home from './views/Home.vue';
+import Reference from "./components/ReferencePage.vue";
+import Addition from "./components/AdditionPage.vue";
+import Delete from "./components/DeletePage.vue";
+import Update from "./components/UpdatePage.vue";
 
 Vue.use(Router);
 
@@ -8,18 +12,38 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   component: Home,
+    // },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+    // },
     {
       path: '/',
-      name: 'home',
-      component: Home,
+      name: 'Reference',
+      component: Reference,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      path: '/add',
+      name: 'add',
+      component: Addition,
+    },
+    {
+      path: '/del',
+      name: 'del',
+      component: Delete,
+    },
+    {
+      path: '/upda',
+      name: 'upda',
+      component: Update,
     },
   ],
 });
