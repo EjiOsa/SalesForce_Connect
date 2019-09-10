82a7d41e293b351dd2c66d83e5844c966e025520
EjiOsa <powder_junky@msn.com>
2019-09-11 05:09:36 +0900

    ページ遷移はツールバーを作成したので各ページのボタンを削除

---
+++ src/components/Pages/ReferencePage.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <v-layout
+    text-center
+    wrap
+    >
+      <v-flex mb-4>
+        <h2>
+          ホーム及びデータの参照をするページ。
+        </h2>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
