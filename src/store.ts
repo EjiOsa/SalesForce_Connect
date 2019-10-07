916471ae9df9529133fb79d208d47212c2a81827
EjiOsa <powder_junky@msn.com>
2019-10-08 06:18:21 +0900

    標準の構文でインクリメントとデクリメントのメソッドを実装。 これを次はクラス構文に変換していく。

--- src/store.ts
+++ src/store.ts
@@ -5,12 +5,28 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    count: 10,
+  },
+  getters: {
   },
   mutations: {
-
+    increment(state) {
+      state.count++;
+    },
+    decrement(state) {
+      state.count--;
+    },
   },
   actions: {
-
+    increment: ({commit}) => {
+      setTimeout(() => {
+        commit('increment');
+      }, 5000);
+    },
+    decrement: ({commit}) => {
+      setTimeout(() => {
+        commit('decrement');
+      }, 1000);
+    },
   },
 });
