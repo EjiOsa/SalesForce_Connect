a187270dc2dbe21436d2b836314d011b85f158da
EjiOsa <powder_junky@msn.com>
2019-10-11 05:10:48 +0900

    flatは不要なのに入れていて、表示はしていたけどエラーがあったので削除した。 toolbarもapp-barが正しかったよう。ちゃんとは調べてない。

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,31 +1,34 @@
 <template>
     <nav>
-        <v-toolbar flat app>
+        <v-app-bar app>
             <v-toolbar-title class="text-uppercase grey--text">
                 <span class="font-weight-light">SALES</span>
                 <span>FORCE</span>
             </v-toolbar-title>
             <v-spacer></v-spacer>
-                <v-btn flat to="/" color="grey">
+                <v-btn to="/" color="grey">
                     <span>REFERENCE</span>
                 </v-btn>
-                <v-btn flat to="/Add" color="grey">
+                <v-btn to="/Add" color="grey">
                     <span>ADDITION</span>
                 </v-btn>
-                <v-btn flat to="/Del" color="grey">
+                <v-btn to="/Del" color="grey">
                     <span>DELETE</span>
                 </v-btn>
-                <v-btn flat to="/Upda" color="grey">
+                <v-btn to="/Upda" color="grey">
                     <span>UPDATE</span>
                 </v-btn>
-        </v-toolbar>
+        </v-app-bar>
     </nav>
 </template>
 
 <script lang="ts">
-export default {
+import {Component, Vue} from 'vue-property-decorator';
 
-};
+@Component
+export default class NavigationBar extends Vue {
+
+}
 </script>
 
 <style>
