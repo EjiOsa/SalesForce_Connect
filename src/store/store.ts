4e6c4355ebe41021032c1d73ca52b184e4851c3a
EjiOsa <powder_junky@msn.com>
2019-10-14 17:19:14 +0900

    types.tsにオブジェクト型の変数を追加。 store.tsでは変数追加に伴って、再度インターフェイスでまとめてからVuex.Storeに投げている。

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,13 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import {ICounterState } from './types';
+import {ICounterState, IBoardStockState } from './types';
 
 Vue.use(Vuex);
 
-// export interface IRootState { //複数のStateを使用するときはここでまとめる。
-  // counterName: ICounterState;
-// }
-// export default new Vuex.Store<IRootState>({});
+export interface IRootState { // 複数のStateを使用するときはここでまとめる。
+  counterName: ICounterState;
+  boardStock: IBoardStockState;
+}
 
-export default new Vuex.Store<ICounterState>({});
+export default new Vuex.Store<IRootState>({});
+// export default new Vuex.Store<ICounterState>({}); // 単数のときはここに直接でもよかった。
--- src/store/types.ts
+++ src/store/types.ts
@@ -1,3 +1,11 @@
 export interface ICounterState {
     count: number;
 }
+
+export interface IBoardStockState {
+    stock: {
+        id: number;
+        name: string;
+        type__c: string;
+    };
+}
