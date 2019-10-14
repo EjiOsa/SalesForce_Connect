9caad30c9d955cd0ad31b008b3b819c426e6cdbc
EjiOsa <powder_junky@msn.com>
2019-10-15 06:10:22 +0900

    オブジェクト型の変数を表示できるように追記。 やや冗長なので、次回はメソッドとかを一つにまとめられるようにする。

--- src/components/Pages/ReferencePage.vue
+++ src/components/Pages/ReferencePage.vue
@@ -12,6 +12,34 @@
         <v-btn @click="decre">-</v-btn>
         <v-card><h1>{{ single }}</h1></v-card>
         <v-card><h1>{{ double }}</h1></v-card>
+        
+        <v-form>
+          <v-container>
+            <v-row>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field
+                  label="Name"
+                  outlined
+                  dense
+                  @change="changeName($event)"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field
+                  label="Type"
+                  outlined
+                  dense
+                ></v-text-field>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-form>
+        
+        <v-card>
+          <h1>{{ sId }}</h1>
+          <h2>{{ sName }}</h2>
+          <h2>{{ sType }}</h2>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
@@ -20,6 +48,7 @@
 <script lang="ts">
 import {Component, Vue, Watch } from 'vue-property-decorator';
 import Counter from '@/store/modules/counter';
+import BoardStock from '@/store/modules/boardStock';
 
 @Component
 export default class ReferencePage extends Vue {
@@ -30,12 +59,28 @@ export default class ReferencePage extends Vue {
   public incre() {
       Counter.increment();
       }
+  public changeName(newName:string ) {
+      BoardStock.changeNameAction(newName);
+      }
+  public changeType(newType:string ) {
+      BoardStock.changeTypeAction(newType);
+      }
 
   get single() {
     return Counter.count;
   }
   get double() {
     return Counter.count * 2;
+  }
+
+  get sId() {
+    return BoardStock.stock.id;
+  }
+  get sName() {
+    return BoardStock.stock.name;
+  }
+  get sType() {
+    return BoardStock.stock.type__c;
   }
 }
 </script>
