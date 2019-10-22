e7eeca4717c4fc7e939241e6e350ee4490f14134
EjiOsa <powder_junky@msn.com>
2019-10-22 18:56:03 +0900

    空白が入っていた。tslintに指摘されたので修正。

--- src/store/types.ts
+++ src/store/types.ts
@@ -2,6 +2,6 @@ export interface ICounterState {
     count: number;
 }
 
-export interface IBoardStockStateList { 
+export interface IBoardStockStateList {
     stock_list: Array<{ id: number; name: string; type__c: string; }>; // 普通の配列
 }
