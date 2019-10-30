78fbf193078cc282f2c45e5897b292b68a9812c7
EjiOsa <powder_junky@msn.com>
2019-10-31 05:59:35 +0900

    @Propでデータを受け取り、それをボタンの名前に反映させる。次回はメソッドの切り替えも同様に実装する。 ここでのbtnNameの後ろの「！」は、これが無いと初期化エラーになるため必要。コメントも次回には削除。 3個書いて、表示非表示にと考えたけど。これの方がスマート。無駄な2個は次回削除。

--- src/components/Parts/BoardDetail.vue
+++ src/components/Parts/BoardDetail.vue
@@ -25,9 +25,25 @@
                     text
                     color="deep-purple accent-4"
                     >
+                    {{ btnName }}
+                    </v-btn>
+                </v-card-actions>
+                <!-- <v-card-actions>
+                    <v-btn
+                    text
+                    color="deep-purple accent-4"
+                    >
                     DELETE
                     </v-btn>
                 </v-card-actions>
+                <v-card-actions>
+                    <v-btn
+                    text
+                    color="deep-purple accent-4"
+                    >
+                    UPDATE
+                    </v-btn>
+                </v-card-actions> -->
             </v-card>
             </v-flex>
         </v-layout>
@@ -35,14 +51,16 @@
 </template>
 
 <script lang="ts">
-import {Component, Vue, Watch } from 'vue-property-decorator';
+import {Component, Vue, Prop } from 'vue-property-decorator';
 import BoardStock from '@/store/modules/boardStock';
 
 @Component
 export default class BoardDetail extends Vue {
+    @Prop({ type: String, required: true, })
+    btnName!: String; // 『！』について。親からのデータを受け取るが、このコンポーネントのみで考えると親からの値は保証されていないので初期化エラーがでる。
 
-get boards() {
-    return BoardStock.BOARD_LIST;
-}
+    get boards() {
+        return BoardStock.BOARD_LIST;
+    }
 }
 </script>
